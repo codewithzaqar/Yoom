7287d71944f08b91b9e118f566ff8e58df869e10
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-04-03 18:00:28 +0400

    Schedule Meeting

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
-
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import 'react-datepicker/dist/react-datepicker.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
