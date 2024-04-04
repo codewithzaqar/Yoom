ada1cf3bfa445efc0e8effb5293c61b7440536a0
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-04-04 22:57:50 +0400

    Join Meeting Modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,6 +8,7 @@ import { useUser } from '@clerk/nextjs'
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { useToast } from './ui/use-toast'
 import ReactDatePicker from 'react-datepicker'
+import { Input } from './ui/input'
 
 const MeetingTypeList = () => {
   const router = useRouter()
@@ -140,6 +141,21 @@ const MeetingTypeList = () => {
            buttonText="Start Meeting"
            handleClick={createMeeting}
         />
+
+        <MeetingModal 
+          isOpen={meetingState === 'isJoiningMeeting'}
+          onClose={() => setMeetingState(undefined)}
+          title='Type the link here'
+          className='text-center'
+          buttonText='Join Meeting'
+          handleClick={() => router.push(values.link)}
+        >
+          <Input
+            placeholder='Meeting link'
+            className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0'
+            onChange={(e) => setValues({ ...values, link: e.target.value})}
+          />
+        </MeetingModal>
     </section>
   )
 }
